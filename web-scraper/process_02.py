457032abd20f9b5e16de88d6d993526506b57aa5
Ben Feifke <benfeifke@gmail.com>
2020-01-23 17:59:22 -0500

    process_02.py gets all verb tenses from the verb conjugation site.

---
+++ web-scraper/process_02.py
@@ -0,0 +1,145 @@
+import requests
+from bs4 import BeautifulSoup
+import ujson as json
+HOME = '../'
+IMPERATIVO_TEMPOS = ['Imperativo Afirmativo', 'Imperativo Negativo']
+BASE_URL = 'https://www.conjugacao.com.br/verbo-'
+SUBJECT_LOOKUP = {
+	'eu': 'eu',
+	'eles': 'eles',
+	'ele': 'ele',
+	'nós': 'nós',
+	'vós': 'vós',
+	'tu': 'tu',
+	'você': 'você',
+	'vocês': 'vocês',
+	'que eu': 'eu',
+	'que eles': 'eles',
+	'que ele': 'ele',
+	'que nós': 'nós',
+	'que vós': 'vós',
+	'que tu': 'tu',
+	'quando eu': 'eu',
+	'quando eles': 'eles',
+	'quando ele': 'ele',
+	'quando nós': 'nós',
+	'quando vós': 'vós',
+	'quando tu': 'tu',
+	'se eu': 'eu',
+	'se eles': 'eles',
+	'se ele': 'ele',
+	'se nós': 'nós',
+	'se vós': 'vós',
+	'se tu': 'tu'
+}
+
+
+def get_page(url):
+	'''Get a webpage given a url; return BeautifulSoup object'''
+	response = requests.get(url)
+	page = BeautifulSoup(response.text, "html5lib")
+	return page
+
+
+def preprocess_verb_string(verb_name):
+	'''function that strips portuguese-specific special characters from the verb-string'''
+	return verb_name.replace('ç', 'c').replace('ô', 'o')
+
+
+def dic_conjugation(conjugation, tense_title):
+	'''make one conjugation into a dictionary'''
+	if tense_title == 'Imperativo Afirmativo':
+		subject_index = 1
+		verb_index = 0
+	elif tense_title == 'Imperativo Negativo':
+		subject_index = 2
+		verb_index = 1
+	else:
+		subject_index = 0
+		verb_index = 1
+	conjugation_parsed = conjugation.find_all('span')
+	conjugation_subject = SUBJECT_LOOKUP[conjugation_parsed[subject_index].string]
+	conjugation_instance = conjugation_parsed[verb_index]
+	conjugation_verb = conjugation_instance.string
+	conjugation_irregular = len(conjugation_instance['class']) - 1
+	return (conjugation_subject, {'conjugation': conjugation_verb, 'irregular': conjugation_irregular})
+
+
+def dic_tense(mood_tense, mood_title):
+	'''make one tense of a mood (e.g. 'presente' of 'indicativo') into a dictionary'''
+
+	# get the title of the tense
+	# possible values: eg. [affirmative, negative] for imperative
+	tense_title = mood_tense.h4.string
+
+	# parse the conjugations
+	conjugations = mood_tense.p.span.findChildren('span', recursive=False)
+	conjugations = conjugations if tense_title not in IMPERATIVO_TEMPOS else conjugations[1:]
+
+	# parse the conjugation set
+	conjugations_parsed = dict([dic_conjugation(conjugation, tense_title)
+								for conjugation in conjugations])
+
+	# return
+	return tense_title, conjugations_parsed
+
+
+def dic_mood(bs4_tense_section):
+	'''make a bs4 tense section a freaking normal data structure cus screw bs4'''
+
+	# get the title of the mood
+	# possible values: [Indicativo, Subjuntivo, Imperativo]
+	mood_title = bs4_tense_section.find("h3", {"class": "modoconjuga"}).string
+
+	# get all the tenses of the mood into a list of tags
+	# possible values: eg. [affirmative, negative] for imperative
+	mood_tenses = bs4_tense_section.find_all(
+		"div", {"class": "tempo-conjugacao"})
+
+	# parse the mood tenses
+	mood_tenses_parsed = dict([dic_tense(mood_tense, mood_title)
+							   for mood_tense in mood_tenses])
+
+	# return
+	return mood_title, mood_tenses_parsed
+
+
+def get_verb(verb_string, dump=True):
+	'''get all the conjugations for a verb'''
+
+	# get the url for querying
+	url = BASE_URL + preprocess_verb_string(verb_string) + '/'
+
+	# get the page
+	page = get_page(url)
+
+	# get all the conjugation 'moods'
+	all_data = page.find_all("div", {"class": "tempos"})
+
+	# we only care about the first three sets: indicativo, subjuntivo, imperativo
+	moods_unparsed = all_data[:3]
+
+	# parse that sheit baby!
+	moods_parsed = dict([dic_mood(mood) for mood in moods_unparsed])
+
+	# output test
+	if dump:
+		dump_verb(verb_string, moods_parsed)
+
+
+def dump_verb(verb_string, verb_conjugation_set):
+	'''save the verb data'''
+
+	# u know it
+	path = HOME + 'data/verb_jsons_02/verbo_' + verb_string + '.json'
+	json.dump(verb_conjugation_set, open(path, 'w'))
+
+
+# main method
+if __name__ == "__main__":
+
+	# verbs to do shit for
+	verbs = json.load(open(HOME + 'data/summary/50_top_verbs.json', 'r'))
+	for verb in verbs:
+		print(verb)
+		get_verb(verb)
