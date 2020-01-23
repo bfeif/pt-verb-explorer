89551463f699819be4303c5ae82c558a4bf603fe
Ben Feifke <benfeifke@gmail.com>
2020-01-23 16:39:22 -0500

    Fixed formatting of web_scraper script.

--- web-scraper/process_01.py
+++ web-scraper/process_01.py
@@ -1,25 +1,23 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 import ujson as json
 HOME = '../'
 IMPERATIVO_TEMPOS = ['Imperativo Afirmativo', 'Imperativo Negativo']
 BASE_URL = 'https://www.conjugacao.com.br/verbo-'
 
-# a function to make one conjugation into a dictionary
-def dic_conjugation(conjugation, subject_index=0, verb_index=1):
 
-	# fuck comments babayyyy
+def dic_conjugation(conjugation, subject_index=0, verb_index=1):
+	'''make one conjugation into a dictionary'''
 	conjugation_parsed = conjugation.find_all('span')
 	conjugation_title = conjugation_parsed[subject_index].string
 	conjugation_instance = conjugation_parsed[verb_index]
 	conjugation_verb = conjugation_instance.string
-	conjugation_irregular = len(conjugation_instance['class'])-1
+	conjugation_irregular = len(conjugation_instance['class']) - 1
 	return (conjugation_title, {'conjugation': conjugation_verb, 'irregular': conjugation_irregular})
 
-# function to make one tense of a mood (e.g. 'presente' of 'indicativo') into a dictionary
+
 def dic_tense(mood_tense):
+	'''make one tense of a mood (e.g. 'presente' of 'indicativo') into a dictionary'''
 
 	# get the title of the tense
 	# possible values: eg. [affirmative, negative] for imperative
@@ -30,30 +28,35 @@ def dic_tense(mood_tense):
 	conjugations = conjugations if tense_title not in IMPERATIVO_TEMPOS else conjugations[1:]
 
 	# parse the conjugation set
-	conjugations_parsed = dict([dic_conjugation(conjugation) for conjugation in conjugations])
+	conjugations_parsed = dict([dic_conjugation(conjugation)
+								for conjugation in conjugations])
 
 	# return
 	return tense_title, conjugations_parsed
 
-# function to make a bs4 tense section a freaking normal data structure cus screw bs4
+
 def dic_mood(bs4_tense_section):
+	'''make a bs4 tense section a freaking normal data structure cus screw bs4'''
 
 	# get the title of the mood
 	# possible values: [Indicativo, Subjuntivo, Imperativo]
-	mood_title = bs4_tense_section.find("h3", { "class" : "modoconjuga" }).string
+	mood_title = bs4_tense_section.find("h3", {"class": "modoconjuga"}).string
 
 	# get all the tenses of the mood into a list of tags
 	# possible values: eg. [affirmative, negative] for imperative
-	mood_tenses = bs4_tense_section.find_all("div", { "class" : "tempo-conjugacao" })
+	mood_tenses = bs4_tense_section.find_all(
+		"div", {"class": "tempo-conjugacao"})
 
 	# parse the mood tenses
-	mood_tenses_parsed = dict([dic_tense(mood_tense) for mood_tense in mood_tenses])
+	mood_tenses_parsed = dict([dic_tense(mood_tense)
+							   for mood_tense in mood_tenses])
 
 	# return
 	return mood_title, mood_tenses_parsed
 
-# get page
+
 def get_page(url):
+	'''get page'''
 
 	# Connect to the URL
 	response = requests.get(url)
@@ -64,12 +67,14 @@ def get_page(url):
 	# return BeautifulSoup object
 	return page
 
-# preprocess verb string
+
 def preprocess_verb_string(verb_name):
+	'''preprocess verb string'''
 	return verb_name.replace('ç', 'c').replace('ô', 'o')
 
-# function that gets all the conjugations for a verb
+
 def get_verb(verb_string):
+	'''get all the conjugations for a verb'''
 
 	# get the url for querying
 	url = BASE_URL + preprocess_verb_string(verb_string) + '/'
@@ -79,9 +84,9 @@ def get_verb(verb_string):
 	# get the page
 	page = get_page(url)
 	print(page)
-	
+
 	# get all the conjugation 'moods'
-	all_data = page.find_all("div", { "class" : "tempos" })
+	all_data = page.find_all("div", {"class": "tempos"})
 
 	# # we only care about the first three sets: indicativo, subjuntivo, imperativo
 	# # just gonna index the first 3 from the list 'all_data' here cus I'm lazy af, but this is unsafe!
@@ -99,12 +104,14 @@ def get_verb(verb_string):
 	# output test
 	dump_verb(verb_string, indicativo_mood_parsed)
 
-# save the verb data
+
 def dump_verb(verb_string, verb_conjugation_set):
+	'''save the verb data'''
 
 	# u know it
 	path = HOME + 'data/verb_data/verbo_' + verb_string + '.json'
 	json.dump(verb_conjugation_set, open(path, 'w'))
+
 
 # main method
 if __name__ == "__main__":
